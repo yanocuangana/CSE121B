7e47b498c15a3bf7088776c1d92b6fb9f661cd7f
yanocuangana <yanocuangana@gmail.com>
2023-10-04 13:33:37 +0100

    add

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -3,30 +3,34 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-const fullname = 'Floriano Mathuca João Cuangana';
-let currentYear = new Date().getFullYear();
-const profilePicture =  'images/yano.jpg'
+
 
 /* Step 3 - Element Variables */
-const foodElement = document.getElementById('food');
-const yearElement = document.querySelector('#year');
-const imageElement = document.querySelector('')
-imageElement.setAttribute('src', profilePicture)
-const nameElement = document.getElementById('name');
-nameElement.innerHTML=`<strong>${fullname}<strong>`;
+
 
 /* Step 4 - Adding Content */
+/* Step 5 - Array */
+const fullname = 'Floriano Mathuca João Cuangana';
+const nameElement = document.querySelector('#name');
+nameElement.textContent = fullname;
+nameElement.innerHTML = `<strong>${fullname}</strong>`;
 
+const currentYear = new Date().getFullYear();
+const yearElement = document.querySelector('#year');
 yearElement.textContent = currentYear;
 
+const picture = 'images/yano.jpg';
+const pictureElement = document.querySelector("#picture");
+pictureElement.setAttribute('src', picture)
 
-
-
-
-/* Step 5 - Array */
-let foodArray = ["pao","Calulu", "Funge", "Feijão de óleo de Palma"]
-foodElement.textContent = foodArray;
-
-
-
-
+const food = ["Funge", "Calulu", "Feijão de oleo de palma", "Mufete"];
+const foodElement = document.querySelector('#food');
+foodElement.textContent = food.join(", ");
+const anotherfood = "Bacalhão com Natas";
+food.push(anotherfood);
+foodElement.textContent = food.join(", ");
+food.shift();
+foodElement.textContent = food.join(", ");
+food.pop();
+foodElement.textContent = food.join(", ");
+//foodElement.textContent = food;
---
+++ w03-task/w03-task.js
@@ -0,0 +1,42 @@
+/* LESSON 3 - Programming Tasks */
+
+/* FUNCTIONS */
+/* Function Definition - Add Numbers */
+ function add(add1, add2){
+    const add1 = document.getElementById("#add1")
+    const add2 = document.getElementById("#add2")
+    const 
+}
+var addNumbers = document.getElementById("#addNumbers")
+addNumbers.addEventListener("click", clicar)
+var sum = document.getElementById("#sum").value = sum
+
+ /* Function Expression - Subtract Numbers */
+ function sub(number1, number2){
+    const sub = 
+ }
+/* Arrow Function - Multiply Numbers */
+ let mul = function(number1, number2){
+    let numb3;
+    numb3 = number1*number2
+    return numb3;
+ }
+
+/* Open Function Use - Divide Numbers */
+
+
+/* Decision Structure */
+
+
+/* ARRAY METHODS - Functional Programming */
+/* Output Source Array */
+
+/* Output Odds Only Array */
+
+/* Output Evens Only Array */
+
+/* Output Sum of Org. Array */
+
+/* Output Multiplied by 2 Array */
+
+/* Output Sum of Multiplied by 2 Array */
